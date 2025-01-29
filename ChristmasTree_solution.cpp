4d4241c3e0766a2fdcfd4375b1f795fc7bc32327
nil0 <le.jeune.nicolas@proton.me>
2025-01-29 12:59:46 +0100

    Add ChristmasTree solution (odd)

---
+++ ChristmasTree_solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int floor = 0;
+    std::cout << "Nb: " << std::endl;
+    std::cin >> floor;
+
+    for (int i = 0; i <= floor; ++i)
+    {   
+        if (i==0)
+        {
+            std::cout << std::string(floor-i, ' ') << std::string(i+1, '*') << std::endl;
+        }
+        else
+        {
+        std::cout << std::string(floor-i, ' ') << std::string(2*i+1, '*') << std::endl;
+        }
+    }
+}
